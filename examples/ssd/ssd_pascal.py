5c72fbb1ac2d49446726624460d97752efdd1c5e
Fan Yang <stoneyang0915@gmail.com>
2016-08-26 13:41:09 +0800

    added lines for triangular learning policy

--- examples/ssd/ssd_pascal.py
+++ examples/ssd/ssd_pascal.py
@@ -325,11 +325,14 @@ num_test_image = 4952
 test_batch_size = 1
 test_iter = num_test_image / test_batch_size
 
-solver_param = {
+# Configure solver
+lr_policy = "triangular"
+if lr_policy == "step":
+  solver_param = {
     # Train parameters
     'base_lr': base_lr,
     'weight_decay': 0.0005,
-    'lr_policy': "step",
+    'lr_policy': lr_policy,
     'stepsize': 40000,
     'gamma': 0.1,
     'momentum': 0.9,
@@ -350,7 +353,35 @@ solver_param = {
     'ap_version': "11point",
     'test_initialization': False,
     }
-
+elif lr_policy == "triangular":
+  solver_param = {
+    # Train parameters
+    'base_lr': base_lr,
+    'max_lr': base_lr * 3.,
+    'start_lr_policy': 0,
+    'weight_decay': 0.0005,
+    'lr_policy': lr_policy,
+    'stepsize': 8276,
+    # 'gamma': 0.1,
+    'momentum': 0.9,
+    # 'iter_size': iter_size,
+    'max_iter': 66208,
+    'snapshot': 23000,
+    'display': 10,
+    # 'average_loss': 10,
+    'type': "SGD",
+    'solver_mode': solver_mode,
+    'device_id': device_id,
+    'debug_info': False,
+    'snapshot_after_train': True,
+    # Test parameters
+    'test_iter': [test_iter],
+    'test_interval': 10000,
+    'eval_type': "detection",
+    'ap_version': "11point",
+    'test_initialization': False,
+    }
+	
 # parameters for generating detection output.
 det_out_param = {
     'num_classes': num_classes,
