44c04fbf55fbb20f3f55df07ba138c94cae5645f
Fan Yang <stoneyang0915@gmail.com>
2016-10-28 17:14:11 +0800

    modified training facilities for ov-5 fast

--- examples/overfeat/train_overfeat.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-
-./build/tools/caffe train \
-    --solver=models/Overfeat/Fast/solver.prototxt \
-    --gpu 0 \
-    2>&1 | tee models/Overfeat/Fast/train_overfeat.log
--- examples/overfeat/train_overfeat_accurate.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-
-./build/tools/caffe train \
-    --solver=models/Overfeat/Accurate/solver.prototxt \
-    --gpu 0 \
-    2>&1 | tee models/Overfeat/Accurate/train_overfeat.log
